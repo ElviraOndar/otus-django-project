3b31be6071a094a2d16649764b48c96669ee401c
Elvira Ondar <elvira_kharunova@mail.ru>
2025-08-28 14:28:29 +0300

    created models for courses

---
+++ courses/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Course, Enrollment
+
+# Register your models here.
+
+
+class EnrollmentInline(admin.TabularInline):
+    model = Enrollment
+    extra = 1  # сколько пустых строк показывается в админ-панели для добавления новых студентов
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'teacher')
+    list_filter = ('teacher',)
+
+    search_fields = ('title', 'description', 'teacher')
+    inlines = [EnrollmentInline]
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'date_joined')
+    list_filter = ('student', 'course', 'date_joined')
+
+    search_fields = ("student__username", "course__title")
+
+
+
+
+
+
---
+++ courses/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="courses_taught",
+        limit_choices_to={"is_teacher": True},
+    )
+    students = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through="Enrollment",
+        related_name="enrolled_courses",
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Enrollment(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["student", "course"], name="uniq_student_course"
+            )
+        ]
+
+    student = models.ForeignKey('users.User',
+                                on_delete=models.CASCADE,
+                                limit_choices_to={'is_student': True},
+                                )
+    course = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student} → {self.course}"
+
+
+
+
+
