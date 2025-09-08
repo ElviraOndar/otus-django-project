58b97ba58754442f27a8b0d076d9aed01851278b
Elvira Ondar <elvira_kharunova@mail.ru>
2025-09-08 17:13:35 +0300

    created footer.html and links_to_pages.html in templates/components

--- courses/urls.py
+++ courses/urls.py
@@ -17,13 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from courses.views import CoursesListView, CourseDetailView, CourseCreateView, CourseUpdateView, CourseDeleteView
+from django.views.generic import TemplateView
+
 
 app_name = 'courses'
 
 urlpatterns = [
-    path('', CoursesListView.as_view(), name='courses'),
-    path('<int:pk>/', CourseDetailView.as_view(), name='course'),
-    path('create/', CourseCreateView.as_view(), name='course_create'),
-    path('<int:pk>/update/', CourseUpdateView.as_view(), name='course_update'),
-    path('<int:pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('courses/', CoursesListView.as_view(), name='courses'),
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course'),
+    path('courses/create/', CourseCreateView.as_view(), name='course_create'),
+    path('courses/<int:pk>/update/', CourseUpdateView.as_view(), name='course_update'),
+    path('courses/<int:pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -39,3 +39,4 @@ class CourseDeleteView(DeleteView):
 
 
 
+
